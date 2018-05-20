7abd0a3826f32f0628bf1ce6375a42f247c5b143
Coen Hyde <coen.hyde@gmail.com>
2018-05-20 16:57:03 -0500

    fixing signing to work with latest version of aws4 and add try catch to signing process

--- index.js
+++ index.js
@@ -6,48 +6,40 @@ var AWS = require('aws-sdk');
 var aws4 = require('aws4');
 var _ = require('lodash');
 
-// Pull credentials from AWS metadata api or env variables
-var chain = new AWS.CredentialProviderChain();
-
 module.exports = function(options) {
   var proxy = proxilate(options);
   proxy.register('backendFetch', function(req, res, options, cb) {
-    chain.resolve(function(err, credentials) {
-      if (err) {
-        err.message = "AWS credentials chain: "+err.message
-        return cb(err);
-      }
+    var targetInfo = url.parse(options.target);
+    var headers = _.clone(req.headers);
+    headers.host = targetInfo.host;
 
-      var targetInfo = url.parse(options.target);
-      var headers = _.clone(req.headers);
-      headers.host = targetInfo.host;
+    // Remove headers from forwarding request that will differ
+    // in new request to backend. This is to help the AWS signature
+    // to match
+    delete headers['connection'];
+    delete headers['x-forwarded-for'];
+    delete headers['x-forwarded-proto'];
+    delete headers['x-forwarded-port'];
 
-      // Remove headers from forwarding request that will differ
-      // in new request to backend. This is to help the AWS signature
-      // to match
-      delete headers['connection'];
-      delete headers['x-forwarded-for'];
-      delete headers['x-forwarded-proto'];
-      delete headers['x-forwarded-port'];
+    var opts = {
+      host: targetInfo.host,
+      path: req.originalUrl,
+      method: req.method,
+      headers: headers,
+    }
 
-      var opts = {
-        host: targetInfo.host,
-        path: req.originalUrl,
-        method: req.method,
-        body: req.body,
-        headers: headers
-      }
+    if (typeof req.body == 'string') {
+      opts.body = req.body;
+    }
 
-      var creds = {
-        accessKeyId: credentials.accessKeyId,
-        secretAccessKey: credentials.secretAccessKey,
-        sessionToken: credentials.sessionToken
-      }
+    try {
+      aws4.sign(opts);
+    } catch(err) {
+      cb(err);
+    }
 
-      var signer = aws4.sign(opts, creds);
-      req.headers = signer.headers;
-      cb();
-    });
+    req.headers = opts.headers;
+    cb();
   });
 
   return proxy;
