5ae04bfb6584e2cba17f09bc478d43cbbeaa3020
Coen Hyde <coen.hyde@gmail.com>
2018-05-20 21:22:32 -0500

    fixing bugs

--- index.js
+++ index.js
@@ -6,40 +6,55 @@ var AWS = require('aws-sdk');
 var aws4 = require('aws4');
 var _ = require('lodash');
 
+// Pull credentials from AWS metadata api or env variables
+var chain = new AWS.CredentialProviderChain();
+
 module.exports = function(options) {
   var proxy = proxilate(options);
   proxy.register('backendFetch', function(req, res, options, cb) {
-    var targetInfo = url.parse(options.target);
-    var headers = _.clone(req.headers);
-    headers.host = targetInfo.host;
+    chain.resolve(function(err, credentials) {
+      if (err) {
+        err.message = "AWS credentials chain: "+err.message
+        return cb(err);
+      }
 
-    // Remove headers from forwarding request that will differ
-    // in new request to backend. This is to help the AWS signature
-    // to match
-    delete headers['connection'];
-    delete headers['x-forwarded-for'];
-    delete headers['x-forwarded-proto'];
-    delete headers['x-forwarded-port'];
+      var targetInfo = url.parse(options.target);
+      var headers = _.clone(req.headers);
+      headers.host = targetInfo.host;
 
-    var opts = {
-      host: targetInfo.host,
-      path: req.originalUrl,
-      method: req.method,
-      headers: headers,
-    }
+      // Remove headers from forwarding request that will differ
+      // in new request to backend. This is to help the AWS signature
+      // to match
+      delete headers['connection'];
+      delete headers['x-forwarded-for'];
+      delete headers['x-forwarded-proto'];
+      delete headers['x-forwarded-port'];
 
-    if (typeof req.body == 'string') {
-      opts.body = req.body;
-    }
+      var opts = {
+        host: targetInfo.host,
+        path: req.originalUrl,
+        method: req.method,
+        headers: headers
+      }
 
-    try {
-      aws4.sign(opts);
-    } catch(err) {
-      cb(err);
-    }
+      if (typeof req.body == 'string') {
+        opts.body = req.body;
+      }
 
-    req.headers = opts.headers;
-    cb();
+      var creds = {
+        accessKeyId: credentials.accessKeyId,
+        secretAccessKey: credentials.secretAccessKey,
+        sessionToken: credentials.sessionToken
+      }
+
+      try {
+        var signer = aws4.sign(opts, creds);
+      } catch(err) {
+        return cb(err);
+      }
+      req.headers = signer.headers;
+      cb();
+    });
   });
 
   return proxy;
